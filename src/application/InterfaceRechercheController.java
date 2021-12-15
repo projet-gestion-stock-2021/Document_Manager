3f2da37362bf36edc199a07cefec2ba973c348aa
ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
2021-12-15 16:04:29 +0100

    switch scene
    Switch entre scene

---
+++ src/application/InterfaceRechercheController.java
@@ -0,0 +1,43 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class InterfaceRechercheController {
+
+	@FXML
+	private Button annulerButton, switchSceneButton;
+	
+	public void switchDashboard () {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+			Stage individuStage = new Stage();
+			
+			individuStage.setScene(new Scene(root,778,605));
+			individuStage.show();
+			Stage stage = (Stage) annulerButton.getScene().getWindow();
+			stage.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.getCause();
+		}
+	}
+	public void switchSceneOnAction () {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("InterfaceCreation.fxml"));
+			Stage individuStage = new Stage();
+			
+			individuStage.setScene(new Scene(root,778,605));
+			individuStage.show();
+			Stage stage = (Stage) switchSceneButton.getScene().getWindow();
+			stage.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.getCause();
+		}
+	}
+}
