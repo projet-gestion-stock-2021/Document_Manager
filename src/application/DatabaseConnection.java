6d4f324b7dd6452725e93ad8a3d167390eeb5d1a
ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
2021-12-20 20:46:01 +0100

    Squashed commit of the following:
    commit 3f2da37362bf36edc199a07cefec2ba973c348aa
    Author: ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
    Date:   Wed Dec 15 16:04:29 2021 +0100
    
        switch scene
    
        Switch entre scene

--- src/application/DatabaseConnection.java
+++ src/application/DatabaseConnection.java
@@ -9,9 +9,9 @@ public class DatabaseConnection {
 	public Connection databaseLink;
 	
 	public Connection getConnection() {
-		String databaseName = "";
-		String databaseUser = "";
-		String databasePassword= "";
+		String databaseName = "examcauwen";
+		String databaseUser = "root";
+		String databasePassword= "Leahpar7";
 		String url = "jdbc:mariadb://localhost:3306/" + databaseName;
 		
 		try {
