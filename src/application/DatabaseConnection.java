5cc69e5cff87ea84c803e14a8218dc0a9bfc059e
Didi Neil <neildidi33@gmail.com>
2021-12-21 19:35:14 +0100

    Squashed commit of the following:
    commit efb04d340c8f4a73eb7ab6cc5fe496a3f79894cc
    Author: Didi Neil <neildidi33@gmail.com>
    Date:   Tue Dec 21 17:06:48 2021 +0100
    
        Singleton connection and stored procedures
    
        Connect via user Login and Password
    
    commit d9abdde0dc2d12d78d552f50a8ae20c481f67c82
    Author: Didi Neil <neildidi33@gmail.com>
    Date:   Tue Dec 21 14:45:51 2021 +0100
    
        Squashed commit of the following:
    
        commit 6d4f324b7dd6452725e93ad8a3d167390eeb5d1a
        Author: ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
        Date:   Mon Dec 20 20:46:01 2021 +0100
    
            Squashed commit of the following:
    
            commit 3f2da37362bf36edc199a07cefec2ba973c348aa
            Author: ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
            Date:   Wed Dec 15 16:04:29 2021 +0100
    
                switch scene
    
                Switch entre scene
    
        commit 7632be4d4246ea8e270484bb865ca6a5aa0db696
        Author: DidiNeil <34097796+DidiNeil@users.noreply.github.com>
        Date:   Tue Dec 14 17:06:41 2021 +0100
    
            Add file DUMP DB

--- src/application/DatabaseConnection.java
+++ src/application/DatabaseConnection.java
@@ -6,25 +6,80 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 	
-	public Connection databaseLink;
+	//singleton
+	private static DatabaseConnection singleConnection;
 	
-	public Connection getConnection() {
-		String databaseName = "examcauwen";
-		String databaseUser = "root";
-		String databasePassword= "Leahpar7";
-		String url = "jdbc:mariadb://localhost:3306/" + databaseName;
+	private  static Connection databaseLink; 
+	
+	//params
+	private static String databaseName = "";
+	private static String databaseUser = "";
+	private static String databasePassword= "";
+	private static String url = "";
+	
+	
+	private DatabaseConnection()
+	{
+		DatabaseConnection.databaseName = "testdm";
+		DatabaseConnection.databaseUser = "root";
+		DatabaseConnection.databasePassword = "Dddd1234";
+		DatabaseConnection.url = "jdbc:mariadb://localhost:3306/" + getDatabaseName();
 		
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			databaseLink = DriverManager.getConnection(url,databaseUser,databasePassword);
-			System.out.print("connecter dans la db");
-		}catch(Exception e) {
-			e.printStackTrace();
-			e.getCause();
+		System.out.println("\t\tInstance creation ! ! !\n");
+	}
+	
+	//Access Singleton
+	public static DatabaseConnection getInstance()
+	{
+		if(singleConnection == null) 
+		{
+			singleConnection = new DatabaseConnection();
 		}
-		
-		return databaseLink;
+		return singleConnection;
 		
 	}
+
+	// connection
+    public  Connection getConnection()
+    {
+
+        if(databaseLink==null)
+        {
+            try 
+            {
+				Class.forName("org.mariadb.jdbc.Driver");
+				databaseLink = DriverManager.getConnection(getUrl(),getDatabaseUser(),getDatabasePassword());
+				System.out.print("connecter dans la db\n");
+				
+				return databaseLink;
+				
+            } 
+            catch(Exception e) 
+            {
+            	e.printStackTrace();
+            	e.getCause();
+            }
+        }
+		return databaseLink;
+    }
+	
+	private static String getDatabaseName() {
+		return databaseName;
+	}
+
+	private static String getDatabaseUser() {
+		return databaseUser;
+	}
+
+	private static String getDatabasePassword() {
+		return databasePassword;
+	}
+
+	private static String getUrl() {
+		return url;
+	}
+	
+	
+	
 
 }
--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -7,10 +7,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-import java.io.File;
-import java.net.URL;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -36,60 +34,82 @@ public class LoginController  {
 
 	
 	
-	public void identifiantButtonOnAction (ActionEvent event) {
+	public void identifiantButtonOnAction (ActionEvent event) 
+	{
 		
 		
-		if(identifiantTextField.getText().isBlank() == false && mdpTextField.getText().isBlank() == false)  {
+		if(identifiantTextField.getText().isBlank() == false && mdpTextField.getText().isBlank() == false)  
+		{
 			validationIdent();
-		} else {
+		} 
+		else 
+		{
 			donneeInvalideLabel.setText("Entrez votre MDP et identifiant");
 		}
 		
 	}
 	
-	public void cancelButtonOnAction(ActionEvent event) {
+	public void cancelButtonOnAction(ActionEvent event) 
+	{
 		Stage stage = (Stage) annulerButton.getScene().getWindow();
 		stage.close(); 
 	}
-	public void validationIdent() {
-		DatabaseConnection connectNow = new DatabaseConnection();
-		Connection connectDb = connectNow.getConnection();
+	
+	public void validationIdent() 
+	{
+//		DatabaseConnection connectNow = new DatabaseConnection();
+//		Connection connectDb = connectNow.getConnection();
 		
-		String verifId= "select count(1) from client where nomClient = '" + mdpTextField.getText().trim() + "' and prenom = '" + identifiantTextField.getText().trim() + "' ";
+		Connection connectDb = DatabaseConnection.getInstance().getConnection();
 		
+		//String query = "select count(1) from client where nomClient = '" + mdpTextField.getText().trim() + "' and prenom = '" + identifiantTextField.getText().trim() + "' ";
+		String query = "{CALL user_identification(?,?)}";
 		
-		try {
+		try 
+		{
 			
-			Statement statement = connectDb.createStatement();
-			ResultSet queryResult = statement.executeQuery(verifId);
+			//Statement statement = connectDb.createStatement();
+			CallableStatement stmt = connectDb.prepareCall(query);
+			stmt.setString(1, identifiantTextField.getText().trim());
+			stmt.setString(2, mdpTextField.getText().trim());
+			
+			ResultSet queryResult = stmt.executeQuery();
 					
-			while(queryResult.next()) {
-				if (queryResult.getInt(1)== 1) {
+			while(queryResult.next()) 
+			{
+				if (queryResult.getInt(1)== 1) 
+				{
 					donneeInvalideLabel.setText("congrats");
 					creerCompte();
 				}
-				else {
+				else 
+				{
 					donneeInvalideLabel.setText("Mauvaise identification");
 				}
 			}
 				
-		} catch(Exception e){
+		} catch(Exception e)
+		{
 			
-			System.out.println("fuck");
-		e.printStackTrace();
+			System.out.println("fuck\n");
+			e.printStackTrace();
 			e.getCause(); 
 		}
 		
 	}
-	public void creerCompte () {
-		try {
+	public void creerCompte () 
+	{
+		try 
+		{
 			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
 			Stage individuStage = new Stage();
 			
 			individuStage.setScene(new Scene(root,778,605));
 			individuStage.show();
 			cancelButtonOnAction(null);
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 			e.getCause();
 		}
