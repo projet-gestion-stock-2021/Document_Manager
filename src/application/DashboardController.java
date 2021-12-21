c714accf69c52907a9b4d5a6853f47fcb596c9c4
YouMer67 <72405886+YouMer67@users.noreply.github.com>
2021-12-21 19:49:50 +0100

    Squashed commit of the following:
    commit 5cc69e5cff87ea84c803e14a8218dc0a9bfc059e
    Author: Didi Neil <neildidi33@gmail.com>
    Date:   Tue Dec 21 19:35:14 2021 +0100
    
        Squashed commit of the following:
    
        commit efb04d340c8f4a73eb7ab6cc5fe496a3f79894cc
        Author: Didi Neil <neildidi33@gmail.com>
        Date:   Tue Dec 21 17:06:48 2021 +0100
    
            Singleton connection and stored procedures
    
            Connect via user Login and Password
    
        commit d9abdde0dc2d12d78d552f50a8ae20c481f67c82
        Author: Didi Neil <neildidi33@gmail.com>
        Date:   Tue Dec 21 14:45:51 2021 +0100
    
            Squashed commit of the following:
    
            commit 6d4f324b7dd6452725e93ad8a3d167390eeb5d1a
            Author: ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
            Date:   Mon Dec 20 20:46:01 2021 +0100
    
                Squashed commit of the following:
    
                commit 3f2da37362bf36edc199a07cefec2ba973c348aa
                Author: ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
                Date:   Wed Dec 15 16:04:29 2021 +0100
    
                    switch scene
    
                    Switch entre scene
    
            commit 7632be4d4246ea8e270484bb865ca6a5aa0db696
            Author: DidiNeil <34097796+DidiNeil@users.noreply.github.com>
            Date:   Tue Dec 14 17:06:41 2021 +0100
    
                Add file DUMP DB
    
    commit 6d4f324b7dd6452725e93ad8a3d167390eeb5d1a
    Author: ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
    Date:   Mon Dec 20 20:46:01 2021 +0100
    
        Squashed commit of the following:
    
        commit 3f2da37362bf36edc199a07cefec2ba973c348aa
        Author: ElijahSoloh <92023598+ElijahSoloh@users.noreply.github.com>
        Date:   Wed Dec 15 16:04:29 2021 +0100
    
            switch scene
    
            Switch entre scene

---
+++ src/application/DashboardController.java
@@ -0,0 +1,47 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class DashboardController {
+	
+	@FXML
+	private Button creationDocumentButton, rechercheDocumentButton, exitDashboard;
+	
+	public void switchInterfaceCreation () {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("InterfaceCreation.fxml"));
+			Stage individuStage = new Stage();
+			
+			individuStage.setScene(new Scene(root,778,605));
+			individuStage.show();
+			Stage stage = (Stage) creationDocumentButton.getScene().getWindow();
+			stage.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.getCause();
+		}
+	}
+	public void switchInterfaceRecherche () {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("InterfaceRecherche.fxml"));
+			Stage individuStage = new Stage();
+			
+			individuStage.setScene(new Scene(root,778,605));
+			individuStage.show();
+			Stage stage = (Stage) rechercheDocumentButton.getScene().getWindow();
+			stage.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.getCause();
+		}
+	}
+	public void cancelButtonOnAction(ActionEvent event) {
+		Stage stage = (Stage) exitDashboard.getScene().getWindow();
+		stage.close(); }
+}
