337d4e94079ae2acafebd0f38da2132c2f7d70f9
ElijahSoloh <eli.cauwen@hotmail.fr>
2022-01-27 20:18:51 +0100

    Update DatabaseConnection.java

--- src/application/DatabaseConnection.java
+++ src/application/DatabaseConnection.java
@@ -22,7 +22,7 @@ public class DatabaseConnection {
 	{
 		DatabaseConnection.databaseName = "testdm";
 		DatabaseConnection.databaseUser = "root";
-		DatabaseConnection.databasePassword = "Dddd1234";
+		DatabaseConnection.databasePassword = "leahpar7";
 		DatabaseConnection.url = "jdbc:mariadb://localhost:3306/" + getDatabaseName();
 		
 		System.out.println("\t\tInstance creation ! ! !\n");
